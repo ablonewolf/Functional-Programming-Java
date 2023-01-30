8a05d526dfc54b21a0eaece567594823a2caa2bf
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-01-30 17:09:19 +0600

    updated the program with lambda expressions.

--- src/FunctionalProgrammingBasic/StreamAList.java
+++ src/FunctionalProgrammingBasic/StreamAList.java
@@ -1,17 +1,13 @@
 package FunctionalProgrammingBasic;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamAList {
     public static void main(String[] args) {
         printEvenNumbersInList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 
-    private static boolean isEven(Integer number) {
-        return number % 2 == 0;
-    }
-
     private static void printEvenNumbersInList(Integer... numbers) {
-        List.of(numbers).stream().filter(StreamAList::isEven).forEach(System.out::println);
+        Stream.of(numbers).filter(number -> number % 2 == 0).forEach(System.out::println);
     }
 }
