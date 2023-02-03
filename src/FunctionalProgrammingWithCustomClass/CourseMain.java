771afca30949977b6a03c262e47af31e0cf62b5b
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-02-03 09:14:39 +0600

    Formatted the code and implemented one or two utility methods.

--- src/FunctionalProgrammingWithCustomClass/CourseMain.java
+++ src/FunctionalProgrammingWithCustomClass/CourseMain.java
@@ -18,31 +18,38 @@ public class CourseMain {
         System.out.println();
         Comparator<Course> comparatorByNumberOfStudents = Comparator.comparing(Course::getNumberOfStudents);
         System.out.println("Sorting all the courses by the number of students");
-        courses
-                .stream()
-                .sorted(comparatorByNumberOfStudents)
-                .forEach(System.out::println);
+        printSortedCourses(courses,comparatorByNumberOfStudents);
         System.out.println();
-
+        System.out.println("Printing the top 3 courses.");
+        printSortedCoursesLimit(courses,comparatorByNumberOfStudents,3);
         Comparator<Course> courseComparatorByNoOfStudentDesc = Comparator
                 .comparing(Course::getNumberOfStudents)
                 .reversed();
         System.out.println("Sorting all the courses by the number of students in decreasing order.");
-        courses
-                .stream()
-                .sorted(courseComparatorByNoOfStudentDesc)
-                .forEach(System.out::println);
-        System.out.println();
+        printSortedCourses(courses,courseComparatorByNoOfStudentDesc);
 
         Comparator<Course> courseComparatorByStudentsAndReview = comparatorByNumberOfStudents
                 .thenComparing(Course::getReviewScore).
                 reversed();
         System.out.println("Sorting all the courses on basis of number of students and review score.");
+        printSortedCourses(courses, courseComparatorByStudentsAndReview);
+
+    }
+
+    private static void printSortedCourses(List<Course> courses, Comparator<Course> courseComparator) {
         courses
                 .stream()
-                .sorted(courseComparatorByStudentsAndReview)
+                .sorted(courseComparator)
                 .forEach(System.out::println);
         System.out.println();
+    }
 
+    private static void printSortedCoursesLimit(List<Course> courses, Comparator<Course> courseComparator, Integer limit) {
+        courses
+                .stream()
+                .sorted(courseComparator)
+                .limit(limit)
+                .forEach(System.out::println);
+        System.out.println();
     }
 }
